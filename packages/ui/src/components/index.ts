ce8bcc9d56abd60ecab76070622ecdf41e4f6ddd
dolphilia <dolphilia@outlook.com>
2025-11-16 15:56:49 +0900

    update

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,6 +1,7 @@
 // コンポーネントのエクスポート
 export { default as Button } from './Button.astro';
 export { default as Card } from './Card.astro';
+export { default as ProjectCard } from './ProjectCard.astro';
 export { default as Navigation } from './Navigation.astro';
 export { default as Sidebar } from './Sidebar.astro';
 export { default as SidebarPersister } from './SidebarPersister.astro';
